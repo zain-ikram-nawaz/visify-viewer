fb3a12d25d72d4644740d7586e1fc9eb26def292
Zain <zainikram704@gmail.com>
2026-06-10 01:30:22 +0500

    vite config updated for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  server: {
+    port: 5173,
+  },
   build: {
     lib: {
       entry: 'src/index.js',
